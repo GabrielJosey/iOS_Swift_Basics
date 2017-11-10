7d6de9c59cffd263083d09e296818444bd47dd16
Acts2024Life@gmail.com <Acts2024Life@Gmail.com>
2017-11-09 19:15:04 -0500

    I added the fizz buzz challenge for future interview questions. Also jotting down and playing with some of the random stuff I am finding in the videos as I come accross them.

--- RefresherExercizes.playground/Pages/InterviewQuestionExamples.xcplaygroundpage/Contents.swift
+++ RefresherExercizes.playground/Pages/InterviewQuestionExamples.xcplaygroundpage/Contents.swift
@@ -1,5 +1,66 @@
 //: [Previous](@previous)
 
+var shoppingCart: [String] = []
+
+func removeLastItem() {
+    shoppingCart.remove(at: 0)
+}
+
+shoppingCart += ["Eggs"]
+
+removeLastItem()
+
+shoppingCart.count
+
+/*
+                     Fizzbuzz challenge
+ 
+-Write a short program that prints each number from 1 to 100 on a new line.
+
+-For each multiple of 3, print "Fizz" instead of the number.
+
+-For each multiple of 5, print "Buzz" instead of the number.
+
+-For numbers which are multiples of both 3 and 5, print "FizzBuzz" instead of the number.
+*/
+
+var multiples: [Int] = []
+
+for number in 1...100 {
+    if number % 3 == 0 {
+        print("fizz")
+    } else if number % 5 == 0 {
+        print("buzz")
+    } else if number % 3 == 0 && number % 5 == 0 {
+        print("fizzbuzz")
+    } else {
+        print("\(number) times celebrating sucess!")
+    }
+}
+
+var x = 0
+
+while x <= 20 {
+    print(x)
+    x += 1
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
